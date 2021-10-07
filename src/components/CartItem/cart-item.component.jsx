20b2587754f9f6d3686d46cf7051fd83d1121c4a
Monika <monika.anna.drozdz@gmail.com>
2021-10-07 21:42:42 +0200

    changing paths of images in cartdropdown component

--- src/components/CartItem/cart-item.component.jsx
+++ src/components/CartItem/cart-item.component.jsx
@@ -16,7 +16,7 @@ const CartItem = ({ item, clearItem }) => {
 		<div className="cart-item">
 			<div
 				className="cart-image"
-				style={{ backgroundImage: `url(${imageUrl})` }}
+				style={{ backgroundImage: `url(${process.env.PUBLIC_URL}${imageUrl})` }}
 			></div>
 			<div className="delete-icon">
 				<ClearIcon
